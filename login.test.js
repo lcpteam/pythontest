03e5c0f7c0d46ec9912174950142523308964deb
lcpteam <87656693+lcpteam@users.noreply.github.com>
2021-09-13 20:27:19 +0800

    Update login.test.js

--- login.test.js
+++ login.test.js
@@ -1,3 +1,25 @@
-test('adds 1 + 2 to equal 3', () => {
-    expect(1+1).toBe(2);
-  });
+const axios = require('axios');
+
+test('use wrong password', () => {
+    return axios.post('http://www.lcptop.com/relations/api/signin', {
+        username: 'admin',
+        password: '333333'
+    }).then(data => {
+        //fail the case
+        expect(1).toBe(2);
+    }).catch(err => {
+        //console.log(err);
+        expect(err.response.status).toBe(401);
+    });
+});
+
+
+test('use correct password', () => {
+    return axios.post('http://www.lcptop.com/relations/api/signin', {
+        username: 'admin',
+        password: '123456'
+    }).then(response => {
+        expect(response.status).toBe(200);
+        expect(response.data.access_token.length).toBeGreaterThan(0);
+    });
+});
