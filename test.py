895b5c5c8577c5a9d8489ab02763a84353fc677a
lcpteam <87656693+lcpteam@users.noreply.github.com>
2021-09-13 12:54:52 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-abc
+#Implementation of Selenium test automation for this Selenium Python Tutorial
+import pytest
+from selenium import webdriver
+import sys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+ 
+def test_lambdatest_todo_app():
+    chrome_driver = webdriver.Chrome()
+    
+    chrome_driver.get('https://lambdatest.github.io/sample-todo-app/')
+    chrome_driver.maximize_window()
+    
+    sleep(2)
+    chrome_driver.close()
